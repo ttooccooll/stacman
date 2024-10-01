c59b4e5ac2a162d1811417f2a229c0e7ac675436
Jason <jasonbohio@gmail.com>
2024-10-01 13:21:38 -0400

    process

--- js/Config.js
+++ js/Config.js
@@ -1,7 +1,7 @@
 Config = Class.extend({
-    API_KEY: "",
-    FEE: 1,
-    BITCOIN_VALUE: 1,
-});
-
-config = new Config();
+    API_KEY: process.env.API_KEY,
+    FEE: process.env.FEE,
+    BITCOIN_VALUE: process.env.BITCOIN_VALUE,
+  });
+  
+  config = new Config();
